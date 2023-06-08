0c8d94d6e531a2319264e63907fd0bdd358604aa
timothysaatum <tsaatum@outlook.com>
2023-06-08 10:44:39 +0000

    added hostels-details

--- hostelier/settings.py
+++ hostelier/settings.py
@@ -91,7 +91,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'honourgh$honour',
+#        'USER': 'honourgh',
+#        'PASSWORD': '4DH2V!xbpjL!9H2',
+#        'HOST': 'honourgh.mysql.pythonanywhere-services.com',
+#        'OPTIONS': {
+#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#        },
+#    }
+#}
 #celery configuration
 #celery -A hostelier worker --loglevel=INFO  -P threads
 CELERY_BROKER_URL = "redis://localhost:6379"
