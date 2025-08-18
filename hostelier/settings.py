872ccfb1a53294347092997c56f8ebdaa3a87604
timothysaatum <tsaatum@outlook.com>
2025-08-18 01:12:55 +0000

    Feat: https security

--- hostelier/settings.py
+++ hostelier/settings.py
@@ -123,6 +123,15 @@ if ENVIRONMENT == 'development':
     }
 
 if ENVIRONMENT == 'production':
+    # HTTPS / Security
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
     DATABASES = {
         'default': dj_database_url.parse(
             config('DATABASE_URL'),
